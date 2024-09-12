04f3df4ef2c64df602243918bbdb8862b5ccaf09
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2024-09-12 20:27:21 +0900

    Update sfewkgipor.lua

--- sfewkgipor.lua
+++ sfewkgipor.lua
@@ -1,3 +1,16 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+	Title = "Scythex is fucked by promax(s)",
+	Text = "ez fucked by promax(s) tooo ez",
+	Duration = 60,
+})
+
+print('SKID EXEC v1.0.2 LOADED')
+print('EZ FUCKED BY PROMAX(s)')
+printidentity()
+print('LV6 HAHA')
+
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local Title = Instance.new("TextLabel")
