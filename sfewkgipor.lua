4339f1d35341a805085e82e6a153c1658d2f6e83
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2024-09-12 19:01:50 +0900

    Create sfewkgipor.lua

---
+++ sfewkgipor.lua
@@ -0,0 +1,7 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+	Title = "Scythex is fucked by promax(s)",
+	Text = "ui = moo exploits, fucked by promax(s)",
+	Duration = 60,
+})
